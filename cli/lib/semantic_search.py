627b31d20bfa37b99fe533b7824bb2e56c256da8
horstrs <horst.marcelo@gmail.com>
2026-01-20 12:05:54 -0300

    added embeding function

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,12 +1,26 @@
 from sentence_transformers import SentenceTransformer
 
-class SemanticSearch():
+
+class SemanticSearch:
     def __init__(self):
-        
-        self.model = SentenceTransformer('all-MiniLM-L6-v2')
-    
-def verify_model():
-    sem_search = SemanticSearch()    
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def generate_embedding(self, text: str):  # -> embeding:
+        if not text or text.isspace():
+            raise ValueError("text for embeding can't be empty")
+        embedding = self.model.encode([text])
+        return embedding[0]
+
+
+def verify_model() -> None:
+    sem_search = SemanticSearch()
     print(f"Model loaded: {sem_search.model}")
     print(f"Max sequence length: {sem_search.model.max_seq_length}")
-        
+
+
+def embed_text(text: str) -> None:
+    sem_search = SemanticSearch()
+    embedding = sem_search.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
-    subparsers.add_parser("verify", help="Check if embedding model was loaded correctly")
+    subparsers.add_parser(
+        "verify", help="Check if embedding model was loaded correctly"
+    )
+
+    embed_text_parser = subparsers.add_parser(
+        "embed_text", help="Generate an embedding for a single text"
+    )
+    embed_text_parser.add_argument(
+        "text", type=str, help="Text to generate the embedding for"
+    )
 
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
+
 
 if __name__ == "__main__":
     main()
