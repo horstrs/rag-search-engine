b67c2b19741a31bf5de0f6e353b002fd6e4eeaca
horstrs <horst.marcelo@gmail.com>
2026-01-23 13:52:52 -0300

    Added summarize option to rag cli

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -13,15 +13,29 @@ def main():
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
 
+    rag_parser = subparsers.add_parser(
+        "summarize",
+        help="Performa an RRF search for a number of movies defined by parameter --limt. Then, generates a summary of the results. The summary is comprehensive, containing several key pieces of information about genre, plot, etc. for each movie",
+    )
+    rag_parser.add_argument("query", type=str, help="Search query for rrf search")
+    rag_parser.add_argument(
+        "--limit",
+        type=int,
+        nargs="?",
+        default=5,
+        const=5,
+        help="Limit of movies to be summarized",
+    )
+
     args = parser.parse_args()
+    query = args.query
+    movies = load_movies()
+    hybrid_search_instance = HybridSearch(movies)
+    gemini_client = GeminiClient()
 
     match args.command:
         case "rag":
-            query = args.query
-            movies = load_movies()
-            hybrid_search_instance = HybridSearch(movies)
-            gemini_client = GeminiClient()
-            results = hybrid_search_instance.rrf_search(query, 5)
+            results = hybrid_search_instance.rrf_search(query, limit=5)
             results = [hits for _, hits in results]
             response = gemini_client.rag(query, results)
             print("Search Results:")
@@ -30,7 +44,16 @@ def main():
                 print(f"  - {title}")
             print("\nRAG response:")
             print(response)
-
+        case "summarize":
+            results = hybrid_search_instance.rrf_search(query, limit=args.limit)
+            results = [hits for _, hits in results]
+            response = gemini_client.summarize(query, results)
+            print("Search Results:")
+            titles = [entry["document"]["title"] for entry in results]
+            for title in titles:
+                print(f"  - {title}")
+            print("\nLLM Summary:")
+            print(response)
         case _:
             parser.print_help()
 
--- cli/lib/gemini_integration.py
+++ cli/lib/gemini_integration.py
@@ -162,3 +162,20 @@ Provide a comprehensive answer that addresses the query:"""
             model=self.model, contents=prompt
         )
         return response.text
+
+    def summarize(self, query: str, docs: list[str]) -> str:
+        prompt = f"""
+Provide information useful to this query by synthesizing information from multiple search results in detail.
+The goal is to provide comprehensive information so that users know what their options are.
+Your response should be information-dense and concise, with several key pieces of information about the genre, plot, etc. of each movie.
+This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+Query: {query}
+Search Results:
+{docs}
+Provide a comprehensive 3-4 sentence answer that combines information from multiple sources:
+"""
+
+        response = self.client.models.generate_content(
+            model=self.model, contents=prompt
+        )
+        return response.text
